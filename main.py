35fe0ff12ab1c76d705b31227011fc98d4559f63
frtgrsl62-dev <frtgrsl62@gmail.com>
2025-09-18 12:55:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ def login_page():
 # text-align: center; → ortalar
     
     #st.title("Giriş Ekranı")
-    st.markdown("<h1 style='color: white;'>Giriş Ekranı</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='color: orange;'>Giriş Ekranı</h1>", unsafe_allow_html=True)
     with st.form("login_form"):
         k_adi = st.text_input("Kullanıcı Adı", key="login_user")
         sifre = st.text_input("Şifre", type="password", key="login_pass")
@@ -442,6 +442,7 @@ elif st.session_state["page"] == "soru":
     soru_goster_page()
 elif st.session_state["page"] == "rapor":
     genel_rapor_page()
+
 
 
 
