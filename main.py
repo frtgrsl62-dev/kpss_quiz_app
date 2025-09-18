00738b3f6957b24b9fffdf7546e8c86a76ee0dff
frtgrsl62-dev <frtgrsl62@gmail.com>
2025-09-18 15:49:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -169,12 +169,12 @@ def ders_secim_page():
 # Konu Seçim Sayfası (Dairesel yüzde gösterimi)
 # ===============================
 def konu_secim_page(ders):
-    st.header(f"{ders} - Konu Seçimi")
+    # st.header(f"{ders} - Konu Seçimi")
     st.markdown(
-    f"<h2 style='color: #ADD8E6; font-size:25px;'>{ders} - Konu Seçimi</h2>",
+    f"<h2 style='color: #B0E0E6; font-size:30px;'>{ders} - Konu Seçimi</h2>",
     unsafe_allow_html=True
     )
-    
+    st.markdown("---")  # alt çizgi ile ayır
     konular = list(soru_bankasi[ders].keys())
     sonuclar = st.session_state.get("sonuclar", {})
 
@@ -464,6 +464,7 @@ elif st.session_state["page"] == "soru":
     soru_goster_page()
 elif st.session_state["page"] == "rapor":
     genel_rapor_page()
+
 
 
 
