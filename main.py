87b2281e4b4ec68770c6766141086319f13a5d40
frtgrsl62-dev <frtgrsl62@gmail.com>
2025-09-25 14:52:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -498,7 +498,7 @@ def genel_rapor_page():
                         with st.expander(f"📑 {konu} Test Detayları"):
                             for test_no, t_sonuc in testler.items():
                                 st.write(f"➡️ {test_no}: ✅ {t_sonuc['dogru']} | ❌ {t_sonuc['yanlis']}")
-        st.markdown("---")
+       #  st.markdown("---")
     if st.button("🏠 Ana Menüye Dön"):
         st.session_state["page"] = "ders"
         st.rerun()
@@ -553,6 +553,7 @@ elif st.session_state["page"] == "rapor":
     genel_rapor_page()
 elif st.session_state["page"] == "profil":
     profil_page()
+
 
 
 
