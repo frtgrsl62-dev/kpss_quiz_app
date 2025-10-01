19674f89ca4d0b73839f0a14988283b455cb32a7
frtgrsl62-dev <frtgrsl62@gmail.com>
2025-10-01 15:36:52 +0300

    Add files via upload

---
+++ ders_konu_notlari.py
@@ -0,0 +1,13 @@
+# ders_konu_notlari.py
+
+ders_konu_notlari = {
+    "Tarih": {
+        "İslamiyet Öncesi Türk Tarihi": "https://drive.google.com/file/d/1PHAx6g-cS0VkF_g7FD8FZc9QNmX2uPK2/view?usp=drive_link",
+        "Türk-İslam Devletleri": "https://drive.google.com/file/d/19R9jf1jmjocqSuIMbYGIxecwq4D973eR/view?usp=drive_link",
+    },
+    "Coğrafya": {
+        "Türkiye'nin Yer Şekilleri": "https://example.com/yer-sekilleri.pdf",
+        "İklim Bilgisi": "https://example.com/iklim.pdf",
+    },
+    # Diğer dersler buraya aynı formatta eklenebilir
+}
