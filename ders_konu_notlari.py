364084ff42e9dcb2c48d665a959bad0f43e24ad4
frtgrsl62-dev <frtgrsl62@gmail.com>
2025-10-01 15:56:09 +0300

    Update ders_konu_notlari.py

--- ders_konu_notlari.py
+++ ders_konu_notlari.py
@@ -1,69 +1,69 @@
 # ders_konu_notlari.py
 
 ders_konu_notlari = {
-  "📖 Türkçe": {
-    "1) Sözcükte Yapı": "https://drive.google.com/file/d/11_yXT1WQuuuHUDCIDfg9ixEBHp0fFqMq/view?usp=drive_link",
-    "2) Sözcük Türleri": "",
-    "3) Söz Dizimi": "",
-    "4) Ses Bilgisi": "",
-    "5) Yazım Kuralları": "",
-    "6) Noktalama İşaretleri": "",
-    "7) Sözcükte Anlam": "",
-    "8) Cümlede Anlam": "",
-    "9) Anlatım Biçimleri": "",
-    "10) Paragraf": "",
-    "11) Anlatım Bozukluğu": "" 
-},
-
- "➗ Matematik": {
- },
-    
-    "📜 Tarih": {
-    "1. İslamiyet Öncesi Türk Tarihi": "https://drive.google.com/file/d/1PHAx6g-cS0VkF_g7FD8FZc9QNmX2uPK2/view?usp=drive_link",
-    "2. İlk Türk-İslam Devletleri ve Beylikleri": "https://drive.google.com/file/d/19R9jf1jmjocqSuIMbYGIxecwq4D973eR/view?usp=drive_link",
-    "3. Türkiye (Anadolu) Selçuklu Devleti ve II. Beylikler Dönemi": "",
-    "4. Osmanlı Devleti Kuruluş ve Yükselme Dönemleri": "",
-    "5. Osmanlı Devleti Kültür ve Medeniyeti": "",
-    "6. 17. yy (Duraklama) ve 18. yy (Gerileme) Dönemleri": "",
-    "7. 19. yy (Dağılma) ve Islahatlar": "",
-    "8. Trablusgarp ve Balkan Savaşları": "",
-    "9. I. Dünya Savaşı ve Sonuçları": "",
-    "10. Milli Mücadele'nin Hazırlık Dönemi": "",
-    "11. Kurtuluş Savaşı Muharebeler Dönemi": "",
-    "12. Atatürk Dönemi İç ve Dış Politika": "",
-    "13. Çağdaş Türk ve Dünya Tarihi": ""
+    "📖 Türkçe": {
+        "1) Sözcükte Yapı": "https://drive.google.com/file/d/11_yXT1WQuuuHUDCIDfg9ixEBHp0fFqMq/view?usp=drive_link",
+        "2) Sözcük Türleri": "",
+        "3) Söz Dizimi": "",
+        "4) Ses Bilgisi": "",
+        "5) Yazım Kuralları": "",
+        "6) Noktalama İşaretleri": "",
+        "7) Sözcükte Anlam": "",
+        "8) Cümlede Anlam": "",
+        "9) Anlatım Biçimleri": "",
+        "10) Paragraf": "",
+        "11) Anlatım Bozukluğu": "" 
     },
 
-"🌍 Coğrafya": {
-    "1) Türkiye'nin Coğrafi Konumu": "",
-    "2) İklim": "",
-    "3) Türkiye'nin Yer Şekilleri": "",
-    "3.1) Dağlar": "",
-    "3.2) Akarsular": "",
-    "3.3) Platolar": "",
-    "3.4) Ovalar": "",
-    "3.5) Göller": "",
-    "3.6) Doğal Afetler": "",
-    "3.7) Dış Kuvvetler": "",
-    "4) Türkiye'de Nüfus": "",
-    "5) Türkiye'de Tarım": "",
-    "6) Türkiye'de Hayvancılık": "",
-    "7) Madenler": "",
-    "8) Sanayi": "",
-    "9) Ulaşım - Ticaret - Turizm": "",
-    "10) Projeler": ""
-},
+    "➗ Matematik": {
+    },
+    
+    "📜 Tarih": {
+        "1. İslamiyet Öncesi Türk Tarihi": "https://drive.google.com/file/d/1PHAx6g-cS0VkF_g7FD8FZc9QNmX2uPK2/view?usp=drive_link",
+        "2. İlk Türk-İslam Devletleri ve Beylikleri": "https://drive.google.com/file/d/19R9jf1jmjocqSuIMbYGIxecwq4D973eR/view?usp=drive_link",
+        "3. Türkiye (Anadolu) Selçuklu Devleti ve II. Beylikler Dönemi": "",
+        "4. Osmanlı Devleti Kuruluş ve Yükselme Dönemleri": "",
+        "5. Osmanlı Devleti Kültür ve Medeniyeti": "",
+        "6. 17. yy (Duraklama) ve 18. yy (Gerileme) Dönemleri": "",
+        "7. 19. yy (Dağılma) ve Islahatlar": "",
+        "8. Trablusgarp ve Balkan Savaşları": "",
+        "9. I. Dünya Savaşı ve Sonuçları": "",
+        "10. Milli Mücadele'nin Hazırlık Dönemi": "",
+        "11. Kurtuluş Savaşı Muharebeler Dönemi": "",
+        "12. Atatürk Dönemi İç ve Dış Politika": "",
+        "13. Çağdaş Türk ve Dünya Tarihi": ""
+    },
 
-"🏛️ Vatandaşlık": {
-    "1) Hukukun Temel Kavramları": "",
-    "2) Anayasa Hukukuna Giriş": "",
-    "3) 1982 Anayasası'nın Genel Esasları": "",
-    "4) Temel Hak ve Ödevler": "",
-    "5) 1982 Anayasası'nda Yasama": "",
-    "6) 1982 Anayasası'nda Yürütme": "",
-    "7) 1982 Anayasası'nda Yargı": "",
-    "8) İdare Hukuku": "",
-    "9) Ulusal ve Uluslararası Kuruluşlar": ""
+    "🌍 Coğrafya": {
+        "1) Türkiye'nin Coğrafi Konumu": "",
+        "2) İklim": "",
+        "3) Türkiye'nin Yer Şekilleri": "",
+        "3.1) Dağlar": "",
+        "3.2) Akarsular": "",
+        "3.3) Platolar": "",
+        "3.4) Ovalar": "",
+        "3.5) Göller": "",
+        "3.6) Doğal Afetler": "",
+        "3.7) Dış Kuvvetler": "",
+        "4) Türkiye'de Nüfus": "",
+        "5) Türkiye'de Tarım": "",
+        "6) Türkiye'de Hayvancılık": "",
+        "7) Madenler": "",
+        "8) Sanayi": "",
+        "9) Ulaşım - Ticaret - Turizm": "",
+        "10) Projeler": ""
+    },
+
+    "🏛️ Vatandaşlık": {
+        "1) Hukukun Temel Kavramları": "",
+        "2) Anayasa Hukukuna Giriş": "",
+        "3) 1982 Anayasası'nın Genel Esasları": "",
+        "4) Temel Hak ve Ödevler": "",
+        "5) 1982 Anayasası'nda Yasama": "",
+        "6) 1982 Anayasası'nda Yürütme": "",
+        "7) 1982 Anayasası'nda Yargı": "",
+        "8) İdare Hukuku": "",
+        "9) Ulusal ve Uluslararası Kuruluşlar": ""
     },
 
     "🌟 Genel Kültür": {
@@ -71,4 +71,3 @@ ders_konu_notlari = {
         "2) Sanat ve Edebiyat": ""
     }
 }
-
