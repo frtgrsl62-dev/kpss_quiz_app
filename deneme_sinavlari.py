527e8b9ec55368530827859fcde269c5f26a79a3
frtgrsl62-dev <frtgrsl62@gmail.com>
2025-10-03 16:53:33 +0300

    Add files via upload

---
+++ deneme_sinavlari.py
@@ -0,0 +1,44 @@
+# deneme_sinavlari.py
+
+deneme_sinavlari = {
+    "Deneme 1": [
+        {
+            "soru": "Osmanlı Devleti hangi yıl yıkılmıştır?",
+            "secenekler": {
+                "A": "1918",
+                "B": "1920",
+                "C": "1922",
+                "D": "1923",
+                "E": "1924"
+            },
+            "dogru_cevap": "C",
+            "cozum": "Saltanat 1922’de kaldırıldı, Osmanlı fiilen sona erdi."
+        },
+        {
+            "soru": "Türkiye Cumhuriyeti hangi yıl ilan edilmiştir?",
+            "secenekler": {
+                "A": "1920",
+                "B": "1921",
+                "C": "1922",
+                "D": "1923",
+                "E": "1924"
+            },
+            "dogru_cevap": "D",
+            "cozum": "29 Ekim 1923’te Cumhuriyet ilan edildi."
+        }
+    ],
+    "Deneme 2": [
+        {
+            "soru": "Kuvayi Milliye birliklerinin en önemli özelliği nedir?",
+            "secenekler": {
+                "A": "Düzenli ordu gibi disiplinli olmaları",
+                "B": "Halktan gönüllülerden oluşmaları",
+                "C": "Yabancı subaylarca yönetilmeleri",
+                "D": "TBMM tarafından kurulmaları",
+                "E": "Yalnızca İstanbul’da savaşmaları"
+            },
+            "dogru_cevap": "B",
+            "cozum": "Kuvayi Milliye gönüllü halk direnişidir."
+        }
+    ]
+}
